6b66b3f7063c39a8367586fa4bd6b524078156a6
Sawsan <33127540+SawsanDaban@users.noreply.github.com>
2022-05-30 17:51:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ for(let i = 0; i<friends.length; i++){
         if(j==1){
             console.log(j + " lines of code in the file, "+ j +" lines of code; "+ friends[i] +" strikes one out, clears it all out, "+"no more lines of code in the file");
         } else {
-            console.log(j + " lines of code in the file, "+ j +" lines of code; "+ friends[i] +" strikes one out, clears it all out, "+ 98 +" lines of code in the file");
+            console.log(j + " lines of code in the file, "+ j +" lines of code; "+ friends[i] +" strikes one out, clears it all out, "+ (j-1) +" lines of code in the file");
         }
     }
 }
