45fc438d7353eb2d997e1ea7321456a786937efa
Sawsan <33127540+SawsanDaban@users.noreply.github.com>
2022-05-31 21:34:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let friends = ["Alex", "Sara", "Bushra", "Mike", "Lucy"];
 
 for(let i = 0; i<friends.length; i++){
+    console.log(friends[i].toUpperCase()+":");
     for(let j=99; j>0;j--){
         if(j==1){
             console.log(j + " lines of code in the file, "+ j +" lines of code; "+ friends[i] +" strikes one out, clears it all out, "+"no more lines of code in the file");
